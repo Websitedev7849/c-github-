42561a0e89fa4026374d1a2bc83cd4f25e08d779
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-04-23 19:43:37 +0530

    Basic data types

---
+++ data-types.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int integer = 2;
+	float floatingNumber = 1.99;
+	double doubleNumber = 1.8888888;
+	bool boolean = false;
+	string str = "this is str";
+	
+	auto autoVar = "";
+	
+	cout << "integer\t" << integer << "\n";
+	cout << "floatingNumber\t" << floatingNumber << "\n";
+	cout << "doubleNumber\t" << doubleNumber << "\n";
+	cout << "boolean\t" << boolean << "\n";
+	cout << "string\t" << str << "\n";
+		
+	cout << autoVar;
+}

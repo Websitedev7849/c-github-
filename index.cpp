b4ec0b38cd139273e63a56d7260a261bd3b4ed01
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-05-31 15:20:07 +0530

    used encapsulation, getters and setters

--- index.cpp
+++ index.cpp
@@ -1,14 +1,38 @@
 #include<iostream>
-using std::cout;
-using std::cin;
-using std::string;
-using std::endl;
+using namespace std;
 
 class Employee {
-public:
+private:
 	string Name;
 	string Title;
 	int Age;
+
+public:
+
+	//getters and setters
+	void setName(string name){
+		Name = name;
+	}
+
+	string getName(){
+		return Name;
+	}
+
+	void setTitle(string title){
+		Title = title;
+	}
+
+	string getTitle(){
+		return Title;
+	}
+	
+	void setAge(int age){
+		Age = age;
+	}
+
+	int getAge(){
+		return Age;
+	}
 
 	void introduceYourSelf(){
 		cout << "Name: " << Name << endl;
@@ -28,8 +52,8 @@ int main(){
 	Employee employee1("siddharth", "Developer", 20);
 	employee1.introduceYourSelf();
 
-	Employee employee2("pawar", "CEO", 20);
-	employee2.introduceYourSelf();
+	employee1.setAge(21);
+	cout << employee1.getName() << " is now " << employee1.getAge() << " years old." << endl; 
 
 	return 0;
 }

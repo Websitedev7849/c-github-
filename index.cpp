2d05868187c377fb3b7aa9412ce8a283f7ce8a80
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-06 19:50:54 +0530

    Q1) A solved

--- index.cpp
+++ index.cpp
@@ -1,17 +1,63 @@
 #include<iostream>
-using std::cout; // this only include cout object
-using std::cin; // this include cin object
-
-namespace customNamespace{
-	int height;
-}
-	
-int main(){
-	
-
-	int height = 5;
-	customNamespace::height = 6;
-	
-	cout << "height outside namespace CustomNamespace is " << height << std::endl;
-	cout << "height inside namespace CustomNamespace is " << customNamespace::height;
+using namespace std;
+#include<conio.h>
+class Volume
+{
+ float V;
+public:
+ void cylinder()
+ {
+  float r,h;
+  cout<<"Enter radius & height of cylinder"<<endl;
+  cin>>r>>h;
+  V=3.14*r*r*h;
+ }
+ void sphere()
+ {
+  float r;
+  cout<<"Enter radius of sphere"<<endl;
+  cin>>r;
+  V=(4*3.14*r*r*r)/3;
+ }
+ void cone()
+ {
+  float r,h;
+  cout<<"Enter radius & height of cone"<<endl;
+  cin>>r>>h;
+  V=(3.14*r*r*h)/3;
+ }
+ void display()
+ {
+  cout<<"Volume is = "<<V<<endl;
+ }
+};
+int main()
+{
+ while(1){
+  int ch,q;
+  Volume ob;
+  cout<<"Select the shape for calculate volume"<<endl<<endl;
+  cout<<"Press 1 for select CYLINDER"<<endl;
+  cout<<"Press 2 for select SPHERE"<<endl;
+  cout<<"Press 3 for select CONE"<<endl<<endl;
+  cin>>ch;
+  cout<<endl;
+   switch(ch)
+     {
+         case 1: ob.cylinder();break;
+         case 2: ob.sphere();break;
+         case 3: ob.cone();break;
+         default: cout<<"Wrong input";
+          
+     }
+  if(ch>=1 && ch<=3)
+   ob.display();
+	cout<<"press any key to countinue... / press 0 to EXIT"<<endl<<endl;
+   q=getch();
+  
+  if(q==48){
+   return 0;
+  }
+  
+ }   
 }

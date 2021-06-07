fdfa3ccc4f5d159a018612a4a777b0ed4864ac58
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-07 13:50:14 +0530

    Q2) B solved

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 const http = require('http');
 const fs = require('fs');
+const empDetails = [
+    {
+        name: "siddharth pawar",
+        dob: "06/08/2001",
+        jd: "05/05/2019",
+        salary: 300000
+    },
+    {
+        name: "pranjal pawar",
+        dob: "25/12/1997",
+        jd: "02/04/2018",
+        salary: 500000
+    }
+];
 const PORT = 3300;
 const server = http.createServer((req,res)=>{
     switch (req.url) {
@@ -16,9 +30,28 @@ const server = http.createServer((req,res)=>{
             })
             break;
         case '/get-info':
-            console.log("request was made to /get-info")
-            console.log(req.body);
-            res.write("request received");
+            let data = '';
+            console.log(`request was made to ${req.url}`);
+            req.on('data', chunk => {
+                data += chunk;
+            });
+            req.on('end', () => {
+                let empToCompare = JSON.parse(data);
+                let employeeFound = empDetails.find((emp)=>{
+                    return emp.name === empToCompare.name &&
+                     emp.dob === empToCompare.dob &&
+                     emp.jd === empToCompare.jd &&
+                     emp.salary === empToCompare.salary;
+                });
+                res.setHeader('Content-Type', 'text/json');
+                if (employeeFound) {
+                    res.write('{"empFound": true}');
+                    res.end();
+                    return;
+                }
+                res.write('{"empFound": false}')
+                res.end();
+            });
             break;
     }
 });

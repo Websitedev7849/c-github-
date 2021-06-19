7b1a1e4c1a9cd459a0470a1477bfd1f26c8e645d
sidd7849 <68861220+sidd7849@users.noreply.github.com>
2021-06-19 15:05:11 +0530

    succefully used custom header file and source code

--- index.cpp
+++ index.cpp
@@ -3,7 +3,10 @@
 using std::cout;
 using std::endl;
 
-
+// task.cpp contains definations
+//task.h contains declarations
+// if we want to include custom header file we use "" instead of <>
+// we also need to include header in task.cpp
 
 int main(){
 	cout << "My lucky number is " << my_lucky_number() << endl;
